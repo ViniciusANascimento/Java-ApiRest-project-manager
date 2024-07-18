dc637cc5809ec4d28327a989038116dde4229e19
Vinicius Nascimento <vini.a.nasci@gmail.com>
2024-07-18 17:59:16 -0300

    feat: add api files

---
+++ src/main/java/org/api_FullStack/ApiFullStackApplication.java
@@ -0,0 +1,13 @@
+package org.api_FullStack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFullStackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/api_FullStack/ApiFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.api_FullStack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFullStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
