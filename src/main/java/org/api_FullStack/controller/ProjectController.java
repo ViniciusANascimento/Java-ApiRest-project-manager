ee46db7a76d3da873df87c98126993489634dcb0
Vinicius Nascimento <vini.a.nasci@gmail.com>
2024-07-19 08:51:29 -0300

    fix: fixed return

--- src/main/java/org/api_FullStack/controller/ProjectController.java
+++ src/main/java/org/api_FullStack/controller/ProjectController.java
@@ -2,11 +2,10 @@ package org.api_FullStack.controller;
 
 import org.api_FullStack.model.Project;
 import org.api_FullStack.service.ProjectService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import java.net.URI;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/project")
@@ -27,12 +26,15 @@ public class ProjectController {
     //Criado desvio para quando utilizado em localhost
     @CrossOrigin(origins = "http:127.0.0.1:5500")
     @PostMapping()
-    public ResponseEntity<Project> save(@RequestBody Project projectToCreate){
-        var projectCreated = projectService.create(projectToCreate);
-        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
-                .path("/{id}")
-                .buildAndExpand(projectCreated.getId())
-                .toUri();
-        return ResponseEntity.created(location).body(projectCreated);
+    public ResponseEntity<?> save(@RequestBody Project projectToCreate){
+        //Criado no modo TryCatch para que a mensagem de retorno seja reaproveitavel.
+        try {
+            var projectCreated = projectService.create(projectToCreate);
+            return new ResponseEntity<>(projectCreated, HttpStatus.CREATED);
+        } catch (NoSuchElementException e ){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        } catch (Exception e){
+            return new ResponseEntity<>("Internal Server Error", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
--- src/main/java/org/api_FullStack/controller/UserController.java
+++ src/main/java/org/api_FullStack/controller/UserController.java
@@ -3,11 +3,13 @@ package org.api_FullStack.controller;
 
 import org.api_FullStack.model.acess.User;
 import org.api_FullStack.service.UserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/users")
@@ -28,13 +30,15 @@ public class UserController {
     }
     @CrossOrigin(origins = "http:127.0.0.1:5500")
     @PostMapping()
-    public ResponseEntity<User> create(@RequestBody User userToCreate){
-        var userCreated = userService.create(userToCreate);
-        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
-                .path("/{id}")
-                .buildAndExpand(userCreated.getId())
-                .toUri();
-        return ResponseEntity.created(location).body(userCreated);
+    public ResponseEntity<?> create(@RequestBody User userToCreate){
+       try {
+           var userCreated = userService.create(userToCreate);
+           return new ResponseEntity<>(userCreated, HttpStatus.CREATED);
+       }catch(NoSuchElementException e){
+           return new ResponseEntity<>(e.getMessage(),HttpStatus.BAD_REQUEST);
+       }catch (Exception e){
+           return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+       }
     }
 }
 
